dbc76d42b9abcb8aa6ba493195931fa45fb754f0
Calum29d <calumdavies08@gmail.com>
2026-04-25 00:10:01 +0100

    fix: update LinkedIn URL in Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export const Footer = () => {
     return (
         <footer id="contact" className="border-t border-white/10 mt-20 py-8 text-center text-white/40 text-xl">
             <div className="flex justify-center gap-12">
-                <span className="hover:text-purple-600"><a href="https://www.linkedin.com/in/calum-davies-051490253/"><i className="fa-brands fa-square-linkedin text-purple-600"></i> Calum Davies</a></span>
+                <span className="hover:text-purple-600"><a href="https://www.linkedin.com/in/calum-davies/"><i className="fa-brands fa-square-linkedin text-purple-600"></i> Calum Davies</a></span>
 
                 <span className="hover:text-purple-600"><a href="https://github.com/Calum29d"><i className="fa-brands fa-square-github text-purple-600"></i> Calum29d</a></span>
             </div>
